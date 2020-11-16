12e1a511b1a87ef9aea731d179e387ddb21ffa42
Gowtham  H D <gowthambhat793@gmail.com>
2020-11-16 23:46:14 +0530

    Movies list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,18 +5,55 @@ import '../Services/css/MovieList.css'
 class MovieList extends Component {
     constructor(props) {
         super(props)
-
+        let movie = getMovies();
+        movie = [...movie];
         this.state = {
-            movie: getMovies()
+            movie: movie
         }
     }
+    // deleteHandler = (id) => {
+    //     const { movie } = this.state;
+    //     const Movie = [...movie];
+    //     let movieInDb = Movie.find(m => m._id === id);
+    //     Movie.splice(Movie.indexOf(movieInDb), 1);
+    //     this.setState({ movie: movieInDb })
+
+
+    // }
 
     render() {
         const { movie } = this.state;
+
+
         return (
-            <div className='starter-template'>
-                {movie.map((movie) => <p>{movie.title}</p>)}
-            </div>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Title</th>
+                        <th scope="col">Genre</th>
+                        <th scope="col">Stock</th>
+                        <th scope="col">Rate</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {movie.map((movie) => {
+                        return <tr>
+                            <th scope="row">{movie.title}</th>
+                            <td>{movie.genre.name}</td>
+                            <td>{movie.numberInStock}</td>
+                            <td>{movie.dailyRentalRate}</td>
+                        </tr>
+                    })}
+
+
+                </tbody>
+            </table>
+
+
+
+
+            // {List}         
         )
     }
 }
