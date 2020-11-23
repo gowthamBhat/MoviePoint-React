d76d2054dd33d8e81a31ad28c303355d252af14d
Gowtham  H D <gowthambhat793@gmail.com>
2020-11-23 23:45:11 +0530

    ListGroup --halfdone

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { getGenres } from '../Services/fakeGenreService'
 function ListGroup(props) {
+    const list = getGenres()
     return (
         <React.Fragment>
             <div className="ListGroup list-group ">
-                <button type="button" className="list-group-item list-group-item-action active">All genres</button>
-                <button type="button" className="list-group-item list-group-item-action">Action </button>
-                <button type="button" className="list-group-item list-group-item-action">Trailer</button>
-                <button type="button" className="list-group-item list-group-item-action">comedy</button>
+                <button type="button" className="list-group-item list-group-item-action active">All Genres</button>
+                {list.map((genres) =>
+                    <button type="button" className="list-group-item list-group-item-action">{genres.name} </button>
+                )}
+
 
             </div>
         </React.Fragment>
